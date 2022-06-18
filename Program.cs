9ca20114c5baea8a38c2acf0aec5ac026261f98b
send-js <69160976+send-js@users.noreply.github.com>
2022-06-18 11:50:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,9 +3,6 @@ using Coinbase.Pro.WebSockets;
 using RabbitMQ.Client;
 using System.Text;
 
-namespace Coinbase.Client.Websocket.Sample
-
-{
     class Program
     {
         static async Task Main()
@@ -101,4 +98,3 @@ namespace Coinbase.Client.Websocket.Sample
             }
         }
     }
-}
